73253f1a3db26ece8563509c97593bc860ae1c25
zheniy314-pixel <zheniy314@icloud.com>
2026-05-15 20:16:32 -0800

    Fix android/app/src/main/java/com/nusha/messenger/MainActivity.java

---
+++ android/app/src/main/java/com/nusha/messenger/MainActivity.java
@@ -0,0 +1,8 @@
+package com.nusha.messenger;
+
+/**
+ * Placeholder - Flutter engine handles activity via manifest entry
+ * The real activity is io.flutter.embedding.android.FlutterActivity
+ */
+public class MainActivity extends io.flutter.embedding.android.FlutterActivity {
+}
